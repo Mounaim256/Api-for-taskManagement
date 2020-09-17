c32a5853bca66aff84630c0b7a3481352b88fe4c
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-17 16:36:32 +0100

    first commit

---
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/ApiForTaskManagementApplication.java
@@ -0,0 +1,13 @@
+package com.spring.taskManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForTaskManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForTaskManagementApplication.class, args);
+	}
+
+}
---
+++ Api-for-taskManagement/src/test/java/com/spring/taskManagement/ApiForTaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.taskManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForTaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
