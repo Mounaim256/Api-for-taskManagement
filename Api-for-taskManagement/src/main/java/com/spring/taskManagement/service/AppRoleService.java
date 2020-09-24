4af9e599237239550831e78c832d3ca526d0a554
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-24 21:35:04 +0100

    Create AppRole Service

--- Api-for-taskManagement/src/main/java/com/spring/taskManagement/service/AppRoleService.java
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/service/AppRoleService.java
@@ -1,13 +1,14 @@
 package com.spring.taskManagement.service;
 
-import java.util.Set;
+import java.util.List;
 
 import com.spring.taskManagement.model.AppRole;
 
 public interface AppRoleService {
-	Set<AppRole> getRoles();
+	List<AppRole> getRoles();
+	List<AppRole> getRolesByName(List<AppRole> roles);
 	AppRole getRole(long id);
-	boolean addRole(AppRole role);
+	AppRole addRole(AppRole role);
 	boolean updateRole(AppRole role);
-	boolean deleteRole(long id);
+	AppRole deleteRole(long id);
 }
--- Api-for-taskManagement/src/main/java/com/spring/taskManagement/service/AppRoleServiceImpl.java
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/service/AppRoleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.spring.taskManagement.service;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,33 +17,49 @@ public class AppRoleServiceImpl implements AppRoleService {
 	private AppRoleRepository roleRepository;
 	
 	@Override
-	public Set<AppRole> getRoles() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<AppRole> getRoles() {
+		return roleRepository.findAll();
 	}
 
 	@Override
 	public AppRole getRole(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<AppRole> role = roleRepository.findById(id);
+		return role.isPresent() ? role.get() : null;
 	}
 
 	@Override
-	public boolean addRole(AppRole role) {
+	public AppRole addRole(AppRole role) {
 		// TODO Auto-generated method stub
-		return roleRepository.save(role) != null ? true : false;
+		return roleRepository.save(role);
 	}
 
 	@Override
 	public boolean updateRole(AppRole role) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean deleteRole(long id) {
-		// TODO Auto-generated method stub
-		return false;
+	public AppRole deleteRole(long id) {
+		Optional<AppRole> role = roleRepository.findById(id);
+		if(role.isPresent()) {
+			roleRepository.delete(role.get());
+			return role.get();
+		}
+		return null;
+	}
+
+	@Override
+	public List<AppRole> getRolesByName(List<AppRole> roles) {
+		List<AppRole> appRoles = new ArrayList<AppRole>();
+		if(roles != null) {
+			roles.forEach(role ->{
+				Optional<AppRole> optional = Optional.ofNullable(roleRepository.findByName(role.getName()));
+				if(optional.isPresent()) {
+					appRoles.add(optional.get());
+				}
+			});
+		}
+		return appRoles;
 	}
 
 }
