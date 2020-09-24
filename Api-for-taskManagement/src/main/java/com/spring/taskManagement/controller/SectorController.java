99a742e7369c44f21d7c367bb51e267ec223379e
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-24 21:32:12 +0100

    Create Sector Controller

---
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/controller/SectorController.java
@@ -0,0 +1,45 @@
+package com.spring.taskManagement.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.taskManagement.model.Sector;
+import com.spring.taskManagement.service.SectorService;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(path="taskManagement")
+public class SectorController {
+
+	@Autowired
+	private SectorService sectorService;
+	
+	@GetMapping("/sectors")
+	public List<Sector> getAllSectors(){
+		return sectorService.getSectors();
+	}
+	
+	@GetMapping("/sectors/{id}")
+	public Sector getSector(@PathVariable Long id){
+		return sectorService.getSector(id);
+	}
+	
+	@PostMapping("/sectors")
+	public Sector addSector(@RequestBody Sector sector){
+		return sectorService.addSector(sector);
+	}
+	
+	@DeleteMapping("/sectors/{id}")
+	public Sector deleteSector(@PathVariable Long id){
+		return sectorService.deleteSector(id);
+	}
+}
