e936d55f4e237bed75fd039e6162994a517f827b
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-24 21:37:21 +0100

    Create Sector Service

--- Api-for-taskManagement/src/main/java/com/spring/taskManagement/service/ProjectServiceImpl.java
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/service/ProjectServiceImpl.java
@@ -1,42 +1,61 @@
 package com.spring.taskManagement.service;
 
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.taskManagement.model.Project;
+import com.spring.taskManagement.repository.ProjectRepository;
 
 @Service
 public class ProjectServiceImpl implements ProjectService {
+	
+	@Autowired
+	private ProjectRepository projectRepository;
 
 	@Override
-	public Set<Project> getProjects() {
-		// TODO Auto-generated method stub
+	public List<Project> getProjects() {
+		return projectRepository.findAll();
+	}
+
+	@Override
+	public Project getProject(Long id) {
+		Optional<Project> project = projectRepository.findById(id);
+		return project.isPresent() ? project.get() : null;
+	}
+
+	@Override
+	public Project addProject(Project project) {
+		if(project != null) {
+			project.setStartDate(LocalDateTime.now());
+			return projectRepository.save(project);
+		}
 		return null;
 	}
 
 	@Override
-	public Project getProject() {
+	public boolean updateProject(Project project) {
 		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Project deleteProject(Long id) {
+		Optional<Project> project = projectRepository.findById(id);
+		if(project.isPresent()) {
+			projectRepository.delete(project.get());
+			return project.get();
+		}
 		return null;
 	}
 
 	@Override
-	public boolean addProject(Project user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean updateProject(Project user) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean deleteProject(int id) {
-		// TODO Auto-generated method stub
-		return false;
+	public Project getProjectByame(String name) {
+		Optional<Project> project = projectRepository.findByName(name);
+		return project.isPresent() ? project.get() : null;
 	}
 
 }
