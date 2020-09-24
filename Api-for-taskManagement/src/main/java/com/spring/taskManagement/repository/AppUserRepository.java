82cea5c9e032c4f4f9e6ed0ccc819287d5e3e233
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-24 21:42:29 +0100

    Create AppUser Repository

--- Api-for-taskManagement/src/main/java/com/spring/taskManagement/repository/AppUserRepository.java
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/repository/AppUserRepository.java
@@ -1,9 +1,11 @@
 package com.spring.taskManagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.spring.taskManagement.model.AppUser;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long>{
-
+	AppUser findByUsername(String name);
 }
