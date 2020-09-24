78664bdacea02f7197ff7f791a7897639221e308
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-24 21:40:57 +0100

    Update AppUser Entity

--- Api-for-taskManagement/src/main/java/com/spring/taskManagement/model/AppUser.java
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/model/AppUser.java
@@ -1,9 +1,8 @@
 package com.spring.taskManagement.model;
 
 import java.io.Serializable;
-import java.util.Set;
+import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -14,11 +13,13 @@ import javax.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class AppUser implements Serializable {
 
 	private static final long serialVersionUID = -3849417663088711283L;
@@ -28,8 +29,8 @@ public class AppUser implements Serializable {
 	private String username;
 	private String password;
 	private boolean active;
-	@ManyToMany(fetch= FetchType.LAZY,cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},mappedBy = "users")
-	private Set<AppRole> roles;
+	@ManyToMany(fetch= FetchType.EAGER)
+	private List<AppRole> roles;
 	
 	
 
