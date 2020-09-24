df6bd3245bd309e346f674d22326fd0aa357b6d3
Mounaim256 <mounaim.lucy@gmail.com>
2020-09-24 21:35:29 +0100

    Create AppUser Controller

--- Api-for-taskManagement/src/main/java/com/spring/taskManagement/controller/AppUserController.java
+++ Api-for-taskManagement/src/main/java/com/spring/taskManagement/controller/AppUserController.java
@@ -1,8 +1,39 @@
 package com.spring.taskManagement.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.taskManagement.model.AppUser;
+import com.spring.taskManagement.service.AppUserService;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(path="/taskManagement")
 public class AppUserController {
-
+	
+	@Autowired
+	private AppUserService userService;
+	
+	@GetMapping("/users")
+	public List<AppUser> getAllUser(){
+		return userService.getUsers();
+	}
+	
+	@GetMapping("/users/{id}")
+	public AppUser getUser(@PathVariable Long id){
+		return userService.getUser(id);
+	}
+	
+	@PostMapping("/users")
+	public AppUser addUser(@RequestBody AppUser user) {
+		return userService.addUser(user);
+	}
 }
